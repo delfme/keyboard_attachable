9441f281d665442da8f4d1351c52a656535944a3
delfme <53510751+delfme@users.noreply.github.com>
2022-11-14 23:12:58 +0100

    Update ios_keyboard_animation_controller.dart

--- lib/src/animation/platform_controllers/ios_keyboard_animation_controller.dart
+++ lib/src/animation/platform_controllers/ios_keyboard_animation_controller.dart
@@ -1,33 +1,32 @@
 import 'package:flutter/animation.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/physics.dart';
 import 'package:keyboard_attachable/src/animation/keyboard_animation_controller.dart';
 
 class IOSKeyboardAnimationController implements KeyboardAnimationController {
   IOSKeyboardAnimationController({required TickerProvider vsync})
-      : _spring = const SpringDescription(mass: 8, stiffness: 1, damping: 4.5),
-        _springVelocity = 10,
-        _controller = AnimationController(vsync: vsync);
+      : //_spring = const SpringDescription(mass: 8, stiffness: 1, damping: 4.5),
+        _spring = const SpringDescription(mass: 3, stiffness: 1, damping: 500),
+        _springVelocity = 20,
+        _delay = 0,
+        _controller = AnimationController(vsync: vsync, duration: const Duration(milliseconds: 50));
 
   final SpringDescription _spring;
   final double _springVelocity;
+  final double _delay;
   final AnimationController _controller;
 
   @override
-  Animation<double> get animation => _controller;
-
-  @override
-  TickerFuture forward() {
-    final forwardSimulation = SpringSimulation(_spring, 0, 1, _springVelocity);
-    return _controller.animateWith(forwardSimulation);
-  }
-
-  @override
-  TickerFuture reverse() {
-    final reverseSimulation = SpringSimulation(_spring, 1, 0, -_springVelocity);
-    return _controller.animateWith(reverseSimulation);
-  }
+  Animation<double> get animation => CurvedAnimation(
+        parent: _controller,
+        curve: Curves.linear,
+      );
 
   @override
   void dispose() => _controller.dispose();
+
+  @override
+  TickerFuture forward() => _controller.forward();
+
+  @override
+  TickerFuture reverse() => _controller.reverse();
 }
